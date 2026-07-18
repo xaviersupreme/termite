cfd368effd2cafca37d818fd679e214af30edae9
xaviersupreme <ricciwut@icloud.com>
2026-07-18 11:36:00 -0700

    Remove the old Direct2D console

--- src/app/termite_resource.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#define IDI_TERMITE 101
--- src/app/windows_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "app/console_window.h"
-
-#include <windows.h>
-
-int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int) {
-    SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
-    termite::console_window application(instance);
-    return application.run();
-}
